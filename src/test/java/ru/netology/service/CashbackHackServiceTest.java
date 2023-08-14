0102ad9ede37b7dc14eaf3e25777d5abab910400
VSmirnovaV <131758107+VSmirnovaV@users.noreply.github.com>
2023-08-14 22:14:27 +0300

    change

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -44,4 +44,13 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
 
     }
+    @Test
+    public void shouldBeCalculateTheAmountWhenBuying1001() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+    }
 }
