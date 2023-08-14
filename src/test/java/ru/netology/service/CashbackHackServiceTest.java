55d436ef234916bbce3d8d7ef138f04bb78d93b1
VSmirnovaV <131758107+VSmirnovaV@users.noreply.github.com>
2023-08-14 22:41:38 +0300

    junit API

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,5 @@
 package ru.netology.service;
 
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -43,6 +42,63 @@ public class CashbackHackServiceTest {
 
         int actual = service.remain(0);
         int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void shouldBeCalculateTheAmountWhenBuying1001() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldBeCalculateBoundaryLess1000Api() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldBeCalculateBoundaryApi() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldBeCalculateTheAmountWhenBuying1Api() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1);
+        int expected = 999;
+        assertEquals(actual, expected);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldBeCalculateTheAmountWhenBuying0Api() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+    @org.junit.jupiter.api.Test
+    public void shouldBeCalculateTheAmountWhenBuying1001Api() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 0;
         assertEquals(actual, expected);
 
     }
